26ebb59032be1ba50555c94cd136bdcf6342fc39
raffo-neotech <r.leonelli@neotech.srl>
2022-10-31 17:48:01 +0100

    aggiunto java_09

---
+++ Java_09/src/Start.java
@@ -0,0 +1,25 @@
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String giocatoreA="carta";
+		String giocatoreB="forbici";
+		
+		if(giocatoreA==giocatoreB) {
+			System.out.println("Pareggio");
+		} else {
+			if(giocatoreA=="sasso"&&giocatoreB=="forbici") {
+				System.out.println("Vince il primo giocatore");
+			} else if (giocatoreA=="carta"&&giocatoreB=="sasso") {
+				System.out.println("Vince il primo giocatore");
+			} else if (giocatoreA=="forbici"&&giocatoreB=="carta") {
+				System.out.println("Vince il primo giocatore");
+			} else {
+				System.out.println("Vince il secondo giocatore");
+			}
+		}
+		
+	}
+
+}
